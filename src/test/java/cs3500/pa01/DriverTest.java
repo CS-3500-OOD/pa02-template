10188aef8ed3efdd889285af090c164be93737d4
Mark Fontenot <mfonten@lyle.smu.edu>
2023-05-17 08:32:51 -0400

    Test update to gradle and build yml

--- src/test/java/cs3500/pa01/DriverTest.java
+++ src/test/java/cs3500/pa01/DriverTest.java
@@ -1,6 +1,6 @@
 package cs3500.pa01;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +8,7 @@ class DriverTest {
 
   @Test
   public void fakeTest() {
+    System.out.println("An important message...");
     assertEquals(5, 5);
   }
 
